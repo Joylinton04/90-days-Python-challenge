2393cab9199ef256ab5c89195a72fb73aa910d13
Joylinton04 <adujoy05@gmail.com>
2025-06-14 09:07:09 +0000

    day 1 py challenge

---
+++ day_1.py
@@ -0,0 +1,15 @@
+#Day 1 of py challenge
+# Script that print hello world and a result of a basic arithmetic operation
+
+
+print("Hello world!")
+
+add = 2+3
+sub = 23-75
+multiply = 43*23
+divide = 100/25
+
+print(add)
+print(sub)
+print(multiply)
+print(divide)
