1c6323e570127ebea3f1850f437532e8d753efbc
Joylinton04 <adujoy05@gmail.com>
2025-06-21 09:45:52 +0000

    py libraries

---
+++ day_10.py
@@ -0,0 +1,10 @@
+# Day 10 of py challenge
+# Write a Python script that calculates the square root of a number using the math library
+
+import math
+
+def squareR(num):
+    return math.sqrt(num)
+
+print(squareR(4))
+
